aa7c5c5db7e1849512a78d21db549e28fcf80734
“matthewhasemeier” <mshweb22@gmail.com>
2022-10-05 20:50:49 -0700

    second commit, button still not woriking, updated and exported api key and ID

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    APP_ID: process.env,     //.a0cdbfd2,  //a0cdbfd2
+    API_KEY: process.env,    //.fd9deec0081577f78062042bee16157e,  //fd9deec0081577f78062042bee16157e
+    BASE_URL: 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={a0cdbfd2}&app_key={fd9deec0081577f78062042bee16157e}',
+    BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application.json',
+
+};
+
+//export APP_ID=:a0cdbfd2
+//export API_KEY=:fd9deec0081577f78062042bee16157e

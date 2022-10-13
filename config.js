7e6799378dc63aa5b46e1b598e90be0a2f17a3d9
“matthewhasemeier” <mshweb22@gmail.com>
2022-10-12 20:58:21 -0700

    final final

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
-    APP_ID: process.env,     //.a0cdbfd2,  //a0cdbfd2
-    API_KEY: process.env,    //.fd9deec0081577f78062042bee16157e,  //fd9deec0081577f78062042bee16157e
-    BASE_URL: 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={a0cdbfd2}&app_key={fd9deec0081577f78062042bee16157e}',
-    BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application.json',
+    APP_ID: process.env.APP_ID,     
+    APP_KEY: process.env.APP_KEY,    
+    BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+    BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
 
 };
 
 //export APP_ID=:a0cdbfd2
 //export API_KEY=:fd9deec0081577f78062042bee16157e
+///us/search/1?app_id={APP_ID}&app_key={API_KEY}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const createServer = require('http').createServer;
 const url = require('url');
 const axios = require('axios');
-const chalk = require('chalk');
-const config = require('./api/frontend/config');
+//const chalk = require('chalk');
+const config = require('./config');
 
 const headers = {
     'Content-Type': 'application/json',
@@ -12,24 +12,26 @@ const headers = {
 
 const decodeParams = searchParams => Array
     .from(searchParams.keys())
-    .reduce((acc,key) => ({ ...acc, [key]: searchParams.get(key)}), {});
+    .reduce((acc, key) => ({ ...acc, [key]: searchParams.get(key)}), {});
 
 const server = createServer((req, res) => {
     const requestURL = url.parse(req.url);
     const decodedParams = decodeParams(new URLSearchParams(requestURL.search));
-    const { search, location, country = 'usa' } = decodedParams;
+    const { search, location, country = 'us' } = decodedParams;
 
-    const targetURL = `${config.base_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
-
+    const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.APP_KEY}&what=${search}&where=${location}`;
+//api_key?
     if (req.method === 'GET') {
-        console.log(chalk.green(`Proxy GET request to : ${targetURL}`));
+        //console.log(chalk.green(`Proxy GET request to : ${targetURL}`))
+        console.log(`Proxy GET request to : ${targetURL}`);
         axios.get(targetURL)
             .then(response => {
                 res.writeHead(200, headers);
                 res.end(JSON.stringify(response.data));
             })
                 .catch(error => {
-                    console.log(chalk.red(error));
+                    //console.log(chalk.red(error))
+                    console.log(error);
                     res.writeHead(500, headers);
                     res.end(JSON.stringify(error));
                 });
@@ -37,5 +39,6 @@ const server = createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-    console.log(chalk.green('Server Listening'));
+    //console.log(chalk.green('Server Listening'))
+    console.log('Server Listening');
 })
